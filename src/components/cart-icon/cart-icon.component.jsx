00ea713275ba1c1bffc551044533902a72939c89
dthapa770 <dthapa@mail.sfsu.edu>
2022-04-27 23:10:19 -0700

    fixed item count misplacement from the shopping icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,6 @@
-import {
-  ShoppingIcon,
-  CartIconContainer,
-  ItemCount,
-} from "./cart-icon.styles.jsx";
+import { CartIconContainer, ItemCount } from "./cart-icon.styles.jsx";
 
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { useContext } from "react";
 import { CartContext } from "../contexts/cart.context";
 const CartIcon = () => {
--- src/components/cart-icon/cart-icon.styles.jsx
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -1,21 +1,17 @@
 import styled from "styled-components";
 
-import { ReactComponent as ShoppingSvg } from "../../assets/shopping-bag.svg";
-
-export const ShoppingIcon = styled(ShoppingSvg)`
-  width: 24px;
-  height: 24px;
-`;
-
 export const CartIconContainer = styled.div`
-  .cart-icon-container {
-    width: 45px;
-    height: 45px;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
+  width: 45px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  svg {
+    width: 24px;
+    height: 24px;
+  }
 `;
 
 export const ItemCount = styled.span`
@@ -24,25 +20,3 @@ export const ItemCount = styled.span`
   font-weight: bold;
   bottom: 12px;
 `;
-
-// .cart-icon-container {
-//   width: 45px;
-//   height: 45px;
-//   position: relative;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   cursor: pointer;
-
-//   .shopping-icon {
-//     width: 24px;
-//     height: 24px;
-//   }
-
-//   .item-count {
-//     position: absolute;
-//     font-size: 10px;
-//     font-weight: bold;
-//     bottom: 12px;
-//   }
-// }
